87139e3bb0db3ae84c90bee8dbc8e76d77be23ea
348588096 <veznup@gmail.com>
2025-05-12 14:32:05 -0400

    Added Apple
    -Movement
    -Speed
    -Random Location

--- MyWorld.java
+++ MyWorld.java
@@ -2,15 +2,23 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     
-    
+    int level = 1;
     
     
     public MyWorld() {
         super(600, 400, 1);
         
+        createApple();
+        
        
         
     }
     
+    public void createApple(){
+        Apple apple = new Apple();
+        apple.setSpeed(level);
+        int x = Greenfoot.getRandomNumber(600);
+        addObject(apple,x,0);
+    }
     
 }
