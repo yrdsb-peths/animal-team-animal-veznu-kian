c770c404b7ce48f51815001bfc4a2b3339eaa9d5
348586678 <348586678@gapps.yrdsb.ca>
2025-05-12 14:33:13 -0400

    updare elephant

--- Elephant.java
+++ Elephant.java
@@ -1,21 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * The elephant - the hero
- * 
- * @author (Kian) 
- * @version May 12th 2025
- */
-public class Elephant extends Actor
-{
-    
-    String facing = "right";
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-
-
 public class Elephant extends Actor
 {
 
