c8f79157c565678647209fc406017fc7e97caa36
348588096 <veznup@gmail.com>
2025-05-12 14:39:16 -0400

    Added a Score Label

--- MyWorld.java
+++ MyWorld.java
@@ -3,12 +3,19 @@ import greenfoot.*;
 public class MyWorld extends World {
     
     int level = 1;
+    Label scoreLabel;
+    public int score = 0;
     
     
     public MyWorld() {
         super(600, 400, 1);
         
+        setBackground("images/background.jpg");
+        
         createApple();
+        
+        scoreLabel = new Label(0,50);
+        addObject(scoreLabel,30,30);
         
        
         
